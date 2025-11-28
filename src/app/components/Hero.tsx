eebcc5e8217a2c0f44a442924b445e8cda920e9f
MyTech2000 <3bdo43ban2023@gmail.com>
2025-11-28 23:30:48 +0200

    Fix HTML entity for apostrophe in Hero component

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -13,7 +13,8 @@ const Hero = () => {
           </div>
           <div>
             <h1 className={`${styles.heroHeadText} text-white`}>
-              {`Hi, I'm`} <span className="text-[#915EFF]">Mohamed Ahmed</span></h1>
+              Hi, I&apos;m <span className="text-[#915EFF]">Mohamed Ahmed</span>
+            </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
               I develop 3D visuals, user <br className="sm:block hidden"/> interfaces and web applications
             </p>
